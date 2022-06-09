29096b0b8ea52da4f79f58529f7c1ec67e19f372
Sergio Lucero <sergiolucero@gmail.com>
2022-06-09 19:25:15 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,12 @@
-import openaq
+#import openaq
 import pandas as pd
 import altair as alt
-
-api = openaq.OpenAQ()
-cdf = api.cities(country='CL', df=True)
-
-df = pd.read_csv('azufre.csv')
-
 import streamlit as st
 
-# azufre tiene toda la data para generar un altair stock plot
+#api = openaq.OpenAQ()
+#cdf = api.cities(country='CL', df=True)
 
+df = pd.read_csv('azufre.csv')
 
 c = alt.Chart(df).mark_line().encode(
     x='fecha',
